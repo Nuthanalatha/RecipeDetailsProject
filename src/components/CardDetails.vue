9d3b419932786783572d66b785f13ea8fe77fb03
Nuthana Gangadharappa <107018@tfcorp.com>
2022-09-23 18:38:34 +0530

    my project commit

---
+++ src/components/CardDetails.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card" style="width: 18rem" @click="DisplayDetails">
+    <div class="card text-center">
+      <div class="card-header">
+        <strong>{{ id }}</strong>
+      </div>
+      <div class="card-header">
+        <strong>{{ name }}</strong>
+      </div>
+      <img :src="img" class="card-img-top" alt="..." />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  emits: ["toggleRecipeStatus"],
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    DisplayDetails() {
+      // this.showRecipe = !this.showRecipe;
+      this.$emit("toggleRecipeStatus", this.id);
+    },
+  },
+};
+</script>
+
+<style>
+.flex-box {
+  display: flex;
+}
+
+.card {
+  flex: 0.2;
+  display: flex;
+  flex-direction: column;
+  margin-left: 5%;
+}
+</style>
---
+++ src/components/DisplayDetails.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="card" style="width: 50rem" v-if="show">
+    <div class="card mb-3">
+      <div class="card-header">
+        <h2>{{ name }}</h2>
+      </div>
+      <img :src="img" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ steps }}
+        </h5>
+        <p>
+          {{ description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    steps: {
+      type: String,
+      required: true,
+    },
+
+    show: {
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  font-weight: bold;
+}
+.card {
+  margin-left: 20%;
+}
+</style>
